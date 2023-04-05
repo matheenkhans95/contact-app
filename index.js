cc451bd9846ed969b97ecd57dbeb989ff7819f9e
matheenkhans95 <98764971+matheenkhans95@users.noreply.github.com>
2023-04-05 18:44:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.handler = (req, res) => {
-  res.status(200).send('Hello, world the changed the version of the code!');
+  res.status(200).send('Hello, world the 3 changed the version of the code!');
 };
 
